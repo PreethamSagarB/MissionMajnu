e6c32e6c299de358a9ca01d6cef353b351b0f1a3
PreethamSagar <preethamsagar.borem9@gmail.com>
2023-02-01 13:40:41 +0530

    feat: removing everything
    jira: main

--- backend/router.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router()
-const Users = require('../Models/User')
-
-router.get('/users',(req,res) =>{
-    Users.fin({},(err,result)=>{
-        res.json(result)
-    })
-})
